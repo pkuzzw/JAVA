c1544c69ec8fde83394f2c60a668f772bb09d18e
pkuzzw <498828398@qq.com>
2019-01-11 20:35:29 +0800

    a test git project

---
+++ MyPro01/src/cn/zzw/test/test001.java
@@ -0,0 +1,11 @@
+package cn.zzw.test;
+
+public class test001 {
+	public static void main(String[] args) {
+		System.out.println("Hello,World");
+		
+		String str="Are you ok";
+		System.out.println(str);
+	}
+
+}
---
+++ hello/src/cn/zzw/test/helloworld.java
@@ -0,0 +1,8 @@
+package cn.zzw.test;
+
+public class helloworld {
+	public static void main() {
+		System.out.println("Hello,World");
+	}
+
+}
